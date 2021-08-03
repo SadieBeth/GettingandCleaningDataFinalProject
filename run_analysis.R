8f62a1886a0f755798ec9907e6285aab383a59fe
SadieBeth <SarahElizabeth.2005@gmail.com>
2021-08-03 12:17:58 -0400

    upload data and test script

---
+++ run_analysis.R
@@ -0,0 +1,15 @@
+#run_analysis.R
+#Merge the training and the test sets to create one data set.
+## load needed libraries
+library(readr)
+
+## load both csv files and set as variables
+pathname<-1
+1
+#Extract only the measurements on the mean and standard deviation for each measurement. 
+
+#Use descriptive activity names to name the activities in the data set
+
+#Appropriately label the data set with descriptive variable names.
+
+#Create a second, independent tidy data set with the average of each variable for each activity and each subject.
